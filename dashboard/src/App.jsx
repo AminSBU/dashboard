9a0ecc1492ae279990d27218718f538f24374718
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-12-12 00:11:24 +0330

    install react pro sidebar

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Home from './Home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
