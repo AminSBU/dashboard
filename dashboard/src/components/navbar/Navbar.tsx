4fd65fa122df4d72dd67560b5a31d822dbbb264e
Amin Bakhshoodeh <bakhshoodeh.amin@meco.mapnagroup.com>
2026-06-21 19:14:40 +0330

    import navbar.scss, add images of navbar

--- dashboard/src/components/navbar/Navbar.tsx
+++ dashboard/src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import "./Navbar.scss"
+
 const Navbar = () => {
     return(
         <div className="navbar">
@@ -6,7 +8,18 @@ const Navbar = () => {
                 <span>website</span>
             </div>
             <div className="icons">
-
+                <img src="/search.svg" alt="" className="icon" />
+                <img src="/app.svg" alt="" className="icon" />
+                <img src="./expand.svg" alt="" className="icon" />
+                <div className="notification">
+                    <img src="./notification.svg" alt="" />
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img src="./user.svg" alt="" />
+                    <span>Amin</span>
+                </div>
+                <img src="./settings.svg" alt="" className="icon" />
             </div>
         </div>
     )
