2cbd6cb7c54ffdebcadfa60546a459628c9a0bf9
Amin Bakhshoodeh <bakhshoodeh.amin@meco.mapnagroup.com>
2026-06-22 18:30:23 +0330

    add some menu items, style design for menu

--- dashboard/src/components/menu/Menu.tsx
+++ dashboard/src/components/menu/Menu.tsx
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
+import "./Menu.scss"
 
 const Menu = () => {
     return(
         <div className="Menu">
             <div className="item">
                 <span className="title">MAIN</span>
-                <Link to="/">
+                <Link to="/" className="listItem">
                     <img src="./home.svg" alt="" />
-                    <span className="listItemTiltle">Home</span>
+                    <span className="listItemTiltle">HomePage</span>
+                </Link>
+                <Link to="/" className="listItem">
+                    <img src="./profile.svg" alt="" />
+                    <span className="listItemTiltle">Profile</span>
+                </Link>
+
+                <span className="title">LISTS</span>
+                <Link to="/" className="listItem">
+                    <img src="./user.svg" alt="" />
+                    <span className="listItemTiltle">Users</span>
+                </Link>
+                <Link to="/" className="listItem">
+                    <img src="./product.svg" alt="" />
+                    <span className="listItemTiltle">product</span>
+                </Link>
+                <Link to="/" className="listItem">
+                    <img src="./order.svg" alt="" />
+                    <span className="listItemTiltle">orders</span>
+                </Link>
+                <Link to="/" className="listItem">
+                    <img src="./post.svg" alt="" />
+                    <span className="listItemTiltle">post</span>
                 </Link>
             </div>
         </div>
