5da2cd065767c00d052e31d4230074876c6fa944
Amin Bakhshoodeh <bakhshoodeh.amin@meco.mapnagroup.com>
2026-06-22 17:26:37 +0330

    modification in menu.tsx

--- dashboard/src/components/menu/Menu.tsx
+++ dashboard/src/components/menu/Menu.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Menu = () => {
     return(
         <div className="Menu">
-            Menu
+            <div className="item">
+                <span className="title">MAIN</span>
+                <Link to="/">
+                    <img src="./home.svg" alt="" />
+                    <span className="listItemTiltle">Home</span>
+                </Link>
+            </div>
         </div>
     )
 }
