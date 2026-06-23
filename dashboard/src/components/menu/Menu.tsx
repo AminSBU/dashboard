b89551ac09cf258addd5cfd5a2b16e80e42ba5ec
Amin Bakhshoodeh <bakhshoodeh.amin@meco.mapnagroup.com>
2026-06-23 12:35:49 +0330

    listItem.title to show, delete comment lines

--- dashboard/src/components/menu/Menu.tsx
+++ dashboard/src/components/menu/Menu.tsx
@@ -11,30 +11,11 @@ const Menu = () => {
                     {item.listItems.map(listItem =>(
                         <Link to="/" className="listItem" key={listItem.id}>
                             <img src={listItem.icon} alt="" />
-                            <span className="listItemTiltle">HomePage</span>
+                            <span className="listItemTiltle">{listItem.title}</span>
                         </Link>
                     ))}
                 </div>
             ))}
-            
-                {/* <span className="title">LISTS</span>
-                <Link to="/" className="listItem">
-                    <img src="./user.svg" alt="" />
-                    <span className="listItemTiltle">Users</span>
-                </Link>
-                <Link to="/" className="listItem">
-                    <img src="./product.svg" alt="" />
-                    <span className="listItemTiltle">product</span>
-                </Link>
-                <Link to="/" className="listItem">
-                    <img src="./order.svg" alt="" />
-                    <span className="listItemTiltle">orders</span>
-                </Link>
-                <Link to="/" className="listItem">
-                    <img src="./post.svg" alt="" />
-                    <span className="listItemTiltle">post</span>
-                </Link> */}
-            {/* </div> */}
         </div>
     )
 }
